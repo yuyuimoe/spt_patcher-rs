6753e8c06782d48703682b890a40a8a930a1241f
Yui <yui@osufx.com>
2025-07-04 15:25:38 -0300

    refactor(validate): refactored bpf validator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use std::{fs::File, io::Read};
+use std::{
+    fs::File,
+    io::{self, Read},
+    path::Path,
+};
 
+#[derive(Debug)]
 struct PatchInfo {
     original_checksum: Vec<u8>,
     original_length: u32,
@@ -7,22 +12,44 @@ struct PatchInfo {
     patched_length: u32,
 }
 
-fn main() {
-    let mut bpf_file_stream =
-        File::open("/home/yui/Repositories/rust/spt_patcher/assets/Assembly-CSharp.dll.bpf")
-            .expect("Something went wrong when trying to read the bpf file");
+fn bpf_validate<P: AsRef<Path>>(path: P) -> io::Result<()> {
+    let mut bpf = File::open(path)?;
 
-    let mut bpf_byba_buf: Vec<u8> = vec![0; 4];
-    bpf_file_stream
-        .read_exact(&mut bpf_byba_buf)
-        .expect("Failed to read bpf magic.");
-
-    if !String::from_utf8(bpf_byba_buf)
-        .expect("Failed to convert the first 4 bytes into string")
-        .eq(&String::from("BYBA"))
-    {
-        panic!("File is not a BPF (No BYBA signature)");
+    let mut header = [0u8; 4];
+    bpf.read_exact(&mut header)?;
+    if header != *b"BYBA" {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            "File is not a BPF file (Invalid Header)",
+        ));
     }
 
-    println!("Success");
+    let mut major_ver = [0u8; 1];
+    bpf.read_exact(&mut major_ver)?;
+    if major_ver[0] != 1 {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            "BPF has invalid major version",
+        ));
+    }
+
+    let mut minor_ver = [0u8; 1];
+    bpf.read_exact(&mut minor_ver)?;
+    if minor_ver[0] != 0 {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            "BPF as invalid minor version",
+        ));
+    }
+
+    Ok(())
+}
+
+fn main() {
+    let file_path =
+        Path::new("/home/yui/Repositories/rust/spt_patcher/assets/Assembly-CSharp.dll.bpf");
+    match bpf_validate(file_path) {
+        Ok(()) => println!("Validation works"),
+        Err(e) => eprintln!("Error: {}", e),
+    };
 }
