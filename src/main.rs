cadba4fe0a97fa23addefff8e6974aa584ec1850
Yui <yui@osufx.com>
2025-07-09 20:51:55 -0300

    feat(patcher): finalized the patcher

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,32 @@ use core::fmt;
 use std::{
     cmp,
     error::Error,
-    fs::File,
+    fs::{self, File},
     io::{self, Read},
     path::Path,
 };
 
+use sha2::{Digest, Sha256};
+
 #[derive(Debug, PartialEq)]
 enum PatcherError {
-    InvalidChecksum,
+    InvalidOriginalChecksum,
+    InvalidPatchedChecksum,
     CouldNotReadFile,
+    AlreadyPatched,
 }
 
 impl fmt::Display for PatcherError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let cause = match *self {
-            PatcherError::InvalidChecksum => {
-                "Input file checksum does not match expectation by the bpf"
+            PatcherError::InvalidOriginalChecksum => {
+                "Input file checksum does not match expectation of the bpf"
+            }
+            PatcherError::InvalidPatchedChecksum => {
+                "Output file checksum does not match expectation of the bpf"
             }
             PatcherError::CouldNotReadFile => "Unable to read file",
+            PatcherError::AlreadyPatched => "Input file is already patched",
         };
         f.write_str(cause)
     }
@@ -110,10 +118,10 @@ fn bpf_generate_info(file: &mut File) -> io::Result<PatchInfo> {
 
     let mut buf_patch_item_count = [0u8; 4];
     file.read_exact(&mut buf_patch_item_count)?;
-    let patched_length = i32::from_le_bytes(buf_patch_item_count);
+    let item_count = i32::from_le_bytes(buf_patch_item_count);
 
     let mut items: Vec<PatchItem> = vec![];
-    for _ in 0..=patched_length {
+    for _ in 0..item_count {
         match PatchItem::from_file(file) {
             Ok(pi) => items.push(pi),
             Err(e) => {
@@ -132,30 +140,45 @@ fn bpf_generate_info(file: &mut File) -> io::Result<PatchInfo> {
     })
 }
 
-fn bpf_patch(input: &mut File, patch_info: PatchInfo) -> Result<bool, PatcherError> {
+fn bpf_patch(input: &mut File, patch_info: PatchInfo) -> Result<Vec<u8>, PatcherError> {
     let mut file_buffer: Vec<u8> = vec![];
     match input.read_to_end(&mut file_buffer) {
         Ok(_) => println!("Input file read succesfully"),
         Err(_) => return Err(PatcherError::CouldNotReadFile),
     };
-    let input_hash = sha256::digest(file_buffer);
-    // println!(
-    //     "{:#?} / {:#?}",
-    //     input_hash,
-    //     String::from_utf8_lossy(&patch_info.original_checksum)
-    // );
-    // if patch_info.original_checksum != input_hash.as_bytes() {
-    //     return Err(PatcherError::InvalidChecksum);
-    // }
-    //
 
-    let patched_data = vec![0u8, patch_info.patched_length];
+    // Hash the input data.
+    let mut hasher = Sha256::new();
+    hasher.update(&file_buffer);
+    let input_hash: [u8; 32] = hasher.finalize().into(); // Convert it to raw bytes
+
+    if patch_info.patched_checksum == input_hash {
+        return Err(PatcherError::AlreadyPatched);
+    }
+
+    if patch_info.original_checksum != input_hash {
+        return Err(PatcherError::InvalidOriginalChecksum);
+    }
+
+    let mut patched_data = vec![0u8; patch_info.patched_length as usize];
     let min_length = cmp::min(patch_info.original_length, patch_info.patched_length);
-    // for item in patch_info.items {
-    //     patched_data[item.offset..=item.data.len()].copy_from_slice(src);
-    // }
 
-    Ok(true)
+    patched_data[..min_length as usize].copy_from_slice(&file_buffer[..min_length as usize]);
+    for item in patch_info.items {
+        let offset = item.offset as usize;
+        patched_data[offset..offset + item.data.len()].copy_from_slice(&item.data);
+    }
+
+    let mut hasher = Sha256::new();
+    hasher.update(&patched_data);
+    let output_hash: [u8; 32] = hasher.finalize().into();
+    if patch_info.patched_checksum != output_hash
+        || patch_info.patched_length != patched_data.len() as i32
+    {
+        return Err(PatcherError::InvalidPatchedChecksum);
+    }
+
+    Ok(patched_data)
 }
 
 fn main() {
@@ -186,7 +209,15 @@ fn main() {
     };
 
     match bpf_patch(&mut assembly_csharp, patch_info) {
-        Ok(_) => println!("File patched in-memory succesfully"),
+        Ok(bytes) => {
+            fs::copy(
+                file_to_be_patched,
+                file_to_be_patched.with_extension("dll.spt-bak"),
+            )
+            .expect("Error cloning input file.");
+            fs::write(file_to_be_patched, bytes).expect("Error replacing input with patched file");
+            println!("File patched succesfully");
+        }
         Err(e) => eprintln!("Error patching file: {}", e),
     };
 }
