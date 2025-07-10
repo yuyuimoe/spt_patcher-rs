4a5e25a84965fe5d263b21db64f01c0ebfa7479e
Yui <yui@osufx.com>
2025-07-10 14:11:17 -0300

    fix(main): relative path instead of literal for assets

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@ use std::{fs, path::Path};
 use spt_patcher::patcher::patch;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let bpf_path =
-        Path::new("/home/yui/Repositories/rust/spt_patcher/assets/Assembly-CSharp.dll.bpf");
-
-    let input_path =
-        Path::new("/home/yui/Repositories/rust/spt_patcher/assets/Assembly-CSharp.dll");
+    let bpf_path = Path::new("assets/Assembly-CSharp.dll.bpf");
+    let input_path = Path::new("assets/Assembly-CSharp.dll");
 
     let output = patch(input_path, bpf_path)?;
+
     fs::copy(input_path, input_path.with_extension("dll.spt-bak"))
         .expect("Error cloning input file.");
     fs::write(input_path, output).expect("Error replacing input with patched file");
